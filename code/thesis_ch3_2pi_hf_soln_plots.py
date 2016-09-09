42a1727b1bf17157b3a3042bf86020c4044dd543
Michael Brown <michael.brown6@my.jcu.edu.au>
2016-09-10 03:34:36 +1000

    More pylint improvements

--- code/thesis_ch3_2pi_hf_soln_plots.py
+++ code/thesis_ch3_2pi_hf_soln_plots.py
@@ -5,13 +5,11 @@ Generate plots for thesis ch 3 on solution of 2PI HF gap equations
 
 Modified from aip2014_poster_plots.py
 
-@author: michael
+@author: Michael Brown
 """
 
-from __future__ import division  # use floating point division by default on python versions < 3.0
 import numpy as np
 import scipy as sc
-
 import pylab as py
 from matplotlib.colors import colorConverter
 
@@ -19,13 +17,13 @@ from common import output_fig
 
 # Figure tweaks for LaTeX from matplotlib cookbook
 # http://wiki.scipy.org/Cookbook/Matplotlib/LaTeX_Examples
-fig_width_pt = 746.25327  # Get this from LaTeX using \showthe\columnwidth
-inches_per_pt = 1.0/72.27               # Convert pt to inch
-golden_mean = (np.sqrt(5)-1.0)/2.0         # Aesthetic ratio
-fig_width = fig_width_pt*inches_per_pt  # width in inches
-fig_height = fig_width*golden_mean      # height in inches
-fig_size =  [fig_width,fig_height]
-params = {'backend': 'ps',
+FIG_WIDTH_PT = 746.25327  # Get this from LaTeX using \showthe\columnwidth
+INCHES_PER_PT = 1.0/72.27               # Convert pt to inch
+GOLDEN_MEAN = (np.sqrt(5)-1.0)/2.0         # Aesthetic ratio
+FIG_WIDTH = FIG_WIDTH_PT*INCHES_PER_PT  # width in inches
+FIG_HEIGHT = FIG_WIDTH*GOLDEN_MEAN      # height in inches
+FIG_SIZE = [FIG_WIDTH, FIG_HEIGHT]
+PARAMS = {'backend': 'ps',
           'lines.linewidth': 2,
           'font.size': 20,
           'axes.labelsize': 20,
@@ -35,10 +33,11 @@ params = {'backend': 'ps',
           'xtick.labelsize': 18,
           'ytick.labelsize': 18,
           'text.usetex': True,
-          'figure.figsize': fig_size}
-py.rcParams.update(params)
+          'figure.figsize': FIG_SIZE}
+py.rcParams.update(PARAMS)
 
 def gap_eq_plots(vb2, mnb2, lam, N, save_figs):
+    """ Make various plots of gap equation solutions for thesis chapter 3. """
     # Numpy array file containing the results array
     # see help(sym_imp_scalars_hartree.driver) for format details, or:
 #    results[:, 0] = Ts
@@ -76,9 +75,10 @@ def gap_eq_plots(vb2, mnb2, lam, N, save_figs):
     # Ts range with a single variable
     _no_si_broken_vev1 = results[:, 1]
     _no_si_broken_vev2 = results[:, 12]
-    no_si_broken_vev = np.where(np.isfinite(_no_si_broken_vev1), _no_si_broken_vev1, _no_si_broken_vev2)
+    no_si_broken_vev = np.where(np.isfinite(_no_si_broken_vev1), _no_si_broken_vev1,
+                                _no_si_broken_vev2)
 
-    no_si_sym_vev= sc.zeros_like(results[:, 4])  # symmetric phase vev identically zero
+    no_si_sym_vev = sc.zeros_like(results[:, 4])  # symmetric phase vev identically zero
     no_si_sym_vev[Ts < crit_T] = sc.nan  # no symmetric phase below the critical temperature
 
     # Higgs mass
@@ -112,48 +112,33 @@ def gap_eq_plots(vb2, mnb2, lam, N, save_figs):
     py.plot(Ts, no_si_broken_mg, 'b-')
     py.plot(Ts, no_si_sym_mn, 'r:')
     py.axis([-1, 250, -1, 550])
-    py.vlines(crit_T, 0, 550, color=colorConverter.to_rgba('0.6',alpha=0.6))
-    py.legend(('$v$(broken)','$v$(symmetric)','$m_H$(broken)','$m_G$(broken)','$m_G=m_H$(symmetric)'), loc = 'best')
+    py.vlines(crit_T, 0, 550, color=colorConverter.to_rgba('0.6', alpha=0.6))
+    py.legend(('$v$(broken)', '$v$(symmetric)', '$m_H$(broken)', '$m_G$(broken)',
+               '$m_G=m_H$(symmetric)'), loc='best')
     py.xlabel('T (MeV)')
     py.ylabel('$v,m_G,m_H$ (MeV)')
     py.grid(False)
-    
-#    py.annotate('Unphysical metastable phase\n(1st order phase transition)', xy=(207, 60),  xycoords='data',
-#                xytext=(-40, 80), textcoords='offset points', ha='center',
-#                size=params['text.fontsize'],
-#                #bbox=dict(boxstyle="round", fc="0.8"),
-#                arrowprops=dict(arrowstyle="fancy",
-#                                fc="0.6", ec="none",
-#                                connectionstyle="angle3,angleA=180,angleB=30"),
-#                )
-    py.annotate('Critical Temperature', xy=(crit_T, 250),  xycoords='data',
+
+    py.annotate('Critical Temperature', xy=(crit_T, 250), xycoords='data',
                 xytext=(150, 0), textcoords='offset points', ha='center',
-                size=params['text.fontsize'],
+                size=PARAMS['text.fontsize'],
                 #bbox=dict(boxstyle="round", fc="0.8"),
                 arrowprops=dict(arrowstyle="fancy",
                                 fc="0.6", ec="none",
                                 connectionstyle="angle3,angleA=90,angleB=180"),
-                )
-#
-#    py.annotate('Goldstone theorem violated', xy=(90, 80),  xycoords='data',
-#                xytext=(-50, -50), textcoords='offset points',
-#                size=params['text.fontsize'],
-#                #bbox=dict(boxstyle="round", fc="0.8"),
-#                arrowprops=dict(arrowstyle="fancy",
-#                                fc="0.6", ec="none",
-#                                connectionstyle="angle3,angleA=0,angleB=90"),
-#                )
+               )
+
     output_fig("twopi-hf-soln.pdf", save_figs, base_path=outputdir, dpi=144)
 
 
 
 if __name__ == "__main__":
-    save_figs = True  # whether to show figures interactively or save to file
+    save_figs = False  # whether to show figures interactively or save to file
     outputdir = "../images/ch3"
 
-    vb2  = 93. ** 2 # Pi-Sigma model
+    vb2 = 93. ** 2 # Pi-Sigma model
     mnb2 = 500. ** 2
-    lam  = 3. * mnb2 / vb2
+    lam = 3. * mnb2 / vb2
     N = 4
     crit_T = np.sqrt(12. * vb2 / (N + 2.))
 
