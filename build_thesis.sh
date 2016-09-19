9b6973783b944993efa3d992ddde660ff322275a
Michael Brown <michael.brown6@my.jcu.edu.au>
2016-09-19 18:09:56 +1000

    Thesis build script (for batch mode pdf generation)

---
+++ build_thesis.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [ -z "$(which lyx | grep 'No lyx in')" ];
+then
+  lyxPath="C:\\Program Files (x86)\\LyX 2.2\\bin\\lyx.exe"
+else
+  lyxPath="lyx"
+fi
+
+fname=thesis-"$(date '+%d%m%Y-%H%M%S')".pdf
+
+"$lyxPath" -E pdf2 $fname thesis_main.lyx
+
